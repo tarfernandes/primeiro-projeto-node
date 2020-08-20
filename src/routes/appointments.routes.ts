b76864b587ac2fdedf018d8c357a77e41c8867d8
tarfernandes <tarcisio_fernandez@hotmail.com>
2020-08-20 01:36:15 -0300

    Iniciando projeto

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {});
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = AppointmentsRepository.findByDate(
+    parsedDate,
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = appointmentsRepository.create(provider, parsedDate);
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
