09bdc72c29a257210181ebb4fa6db73749c76906
tarfernandes <tarcisio_fernandez@hotmail.com>
2020-10-27 17:05:22 -0300

    27/10/2020

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,3 +1,48 @@
-import Appointment from '../models/Appointment';
+import { startOfHour } from 'date-fns';
 
-class CreateAppointmentService {}
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+/**
+ * [x] Recebimento das informações
+ * [/] Tratativa de erros/excessões
+ * [x]  Acesso ao repositório
+ */
+
+// SOLID
+
+// SINGLE RESPONSABILITY PRINCIPLE
+// DEPENDENCY INVERTION PRINCIPLE
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ date, provider }: Request): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
